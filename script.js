3d6989a41693781198c18b9cd870853fa42b47b6
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-03-06 17:19:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function isFibonacci(num) {
 		// Write your code here
+	 if(num ===0 || num===1){
+		 retrun true
+	 }
+	let a = 0;
+	let b = 1;
+	let fib = 0;
+	while(true){
+		fib= a+b
+		a = b;
+	    b= fib
+		if(fib == num){
+			return true
+		}
+		else if(fib>=n) {
+		return false
+		}
+		
 }
+	 
+}
+
 const input = parseInt(prompt("Enter a number"));
 alert(isFibonacci(input));
