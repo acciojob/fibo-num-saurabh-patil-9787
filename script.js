b2d47fe3e22c260fce4838ccf6a3379fe5d2b4e8
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-03-06 17:30:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function isFibonacci(num) {
-		// Write your code here
-	 if(num ===0 || num===1){
-		 retrun true
-	 }
-	let a = 0;
-	let b = 1;
-	let fib = 0;
-	while(true){
-		fib= a+b
-		a = b;
-	    b= fib
-		if(fib == num){
-			return true
-		}
-		else if(fib>=n) {
-		return false
-		}
-		
+    // Write your code here
+ if(num ==0 || num== 1){
+     return true
+ }
+let a = 0;
+let b = 1;
+let fib = 0;
+while(true){
+    fib= a+b
+    a = b;
+    b= fib;
+    if(fib == num){
+        return true
+    }
+    else if(fib>=num) {
+    return false
+    }
+    
 }
-	 
+ 
 }
 
 const input = parseInt(prompt("Enter a number"));
